082734f3a183e8063b71f70b4b39e31534aeebfd
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-24 12:29:47 -0400

    cleaned up code formatting

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -7,40 +7,40 @@ const containerStyle = {
     height: '100%',
   };
   
-  const center = {
-    lat: 42.3601,
-    lng: -71.0589,
-  };
+const center = {
+  lat: 42.3601,
+  lng: -71.0589,
+};
 
-  const mapOptions = {
-    disableDefaultUI: true,
-    styles: [
-      {
-        featureType: 'poi',
-        elementType: 'labels',
-        stylers: [{ visibility: 'off' }],
-      },
-      {
-        featureType: 'transit',
-        elementType: 'all',
-        stylers: [{ visibility: 'off' }],
-      },
-    ],
-  };
+const mapOptions = {
+  disableDefaultUI: true,
+  styles: [
+    {
+      featureType: 'poi',
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }],
+    },
+    {
+      featureType: 'transit',
+      elementType: 'all',
+      stylers: [{ visibility: 'off' }],
+    },
+  ],
+};
 
-  const markers = [
-    { lat: 42.3601, lng: -71.0589 },
-    { lat: 42.3331, lng: -71.0897 },
-    { lat: 42.3772, lng: -71.1167 },
-  ];
+const markers = [
+  { lat: 42.3601, lng: -71.0589 },
+  { lat: 42.3331, lng: -71.0897 },
+  { lat: 42.3772, lng: -71.1167 },
+];
 
-  const markerIcon = {
-    path: 'M-8,0a8,8 0 1,0 16,0a8,8 0 1,0 -16,0',
-    fillColor: '#FF0000',
-    fillOpacity: 0.9,
-    strokeWeight: 0,
-    scale: 0.5,
-  };
+const markerIcon = {
+  path: 'M-8,0a8,8 0 1,0 16,0a8,8 0 1,0 -16,0',
+  fillColor: '#FF0000',
+  fillOpacity: 0.9,
+  strokeWeight: 0,
+  scale: 0.5,
+};
 
 export default function InteractiveMap() {
   return (
