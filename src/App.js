f9a4555b8ba42b71d72d39c6cfec498db4683114
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-23 21:48:51 -0400

    added in BostonMap and Results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import BostonMap from './components/BostonMap';
+import Results from './components/Results';
 import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
+      <BostonMap/>
+      <Results/>
     </div>
   );
 }
