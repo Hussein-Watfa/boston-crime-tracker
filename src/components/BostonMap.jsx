2d5c8e6941cc8e2cb52a7049746808d3211e100a
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-24 12:29:09 -0400

    edited file

--- src/components/BostonMap.jsx
+++ src/components/BostonMap.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import './BostonMap.css'
 import InteractiveMap from './InteractiveMap';
+import Legend from './Legend';
 
 export default function BostonMap() {
   return (
     <div className = "BostonMap">
         <InteractiveMap/>
-
-        <div className = "Legend">
-        <select className="Legend-dropdown">
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-            <option value="option4">Option 4</option>
-        </select>
-        </div>
+        <Legend/>
     </div>
   )
 }
