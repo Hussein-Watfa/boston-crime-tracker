ca40fdea90b6e6531fb19d526aaab2c2d43e8cc6
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-23 21:49:46 -0400

    created BostonMap

---
+++ src/components/BostonMap.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BostonMap.css'
+
+export default function BostonMap() {
+  return (
+    <div className = "BostonMap">
+        <div className = "Map">
+            
+        </div>
+
+        <div className = "Legend">
+
+        </div>
+    </div>
+  )
+}
