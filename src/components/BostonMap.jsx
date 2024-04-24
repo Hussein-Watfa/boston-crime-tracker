744cc3e05c2548310df2e865928625487939aa01
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-23 23:37:23 -0400

    reworked the file

--- src/components/BostonMap.jsx
+++ src/components/BostonMap.jsx
@@ -1,62 +1,19 @@
 import React from 'react'
 import './BostonMap.css'
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
-
-const containerStyle = {
-    width: '100%',
-    height: '100%',
-  };
-  
-  const center = {
-    lat: 42.3601,
-    lng: -71.0589,
-  };
-
-  const mapOptions = {
-    disableDefaultUI: true,
-    styles: [
-      {
-        featureType: 'poi',
-        elementType: 'labels',
-        stylers: [{ visibility: 'off' }],
-      },
-      {
-        featureType: 'transit',
-        elementType: 'all',
-        stylers: [{ visibility: 'off' }],
-      },
-    ],
-  };
-
-  const markers = [
-    { lat: 42.3601, lng: -71.0589 },
-    { lat: 42.3331, lng: -71.0897 },
-    { lat: 42.3772, lng: -71.1167 },
-  ];
-
-  const markerIcon = {
-    path: 'M-8,0a8,8 0 1,0 16,0a8,8 0 1,0 -16,0',
-    fillColor: '#FF0000',
-    fillOpacity: 0.9,
-    strokeWeight: 0,
-    scale: 0.5,
-  };
+import InteractiveMap from './InteractiveMap';
 
 export default function BostonMap() {
   return (
     <div className = "BostonMap">
-        <div className = "Map">
-            <LoadScript googleMapsApiKey = 'AIzaSyAjZlv21IG9xvh5n_TE1K2VqaOVAp3V6_I' loadingElement={<div>Loading...</div>}>
-            <GoogleMap mapContainerStyle = {containerStyle} center = {center} zoom = {11} options={mapOptions}>
-                {markers.map((marker, index) => (
-                    <MarkerF key={index} position={marker} icon = {markerIcon} />
-                ))}     
-            </GoogleMap>
-            </LoadScript>
-        </div>
+        <InteractiveMap/>
 
         <div className = "Legend">
-
+        <select className="Legend-dropdown">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+            <option value="option4">Option 4</option>
+        </select>
         </div>
     </div>
   )
