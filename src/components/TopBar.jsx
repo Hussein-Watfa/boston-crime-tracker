cea000f2c764d53e9e40eae71da028a0e843af54
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-23 21:17:53 -0400

    added title

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './TopBar.css'
 
 export default function TopBar() {
   return (
     <div className = "TopBar" >
-      
+      <text>Boston Crime Tracker</text>
     </div>
   )
 }
