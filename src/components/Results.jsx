bc502df43581a71898ea3521bdd10fc9896bc7b0
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-24 20:42:32 -0400

    added title

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,7 +4,7 @@ import './Results.css'
 export default function Results() {
   return (
     <div className = "Results">
-      
+      <text className = "text-Results">Top 5 Recent Cases</text>
     </div>
   )
 }
