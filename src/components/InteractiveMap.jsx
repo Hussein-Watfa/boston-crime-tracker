81dd12d48a2c8fd26f6a8d79a1d2830f7bcbdb95
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-24 21:48:32 -0400

    added the data provider for the context

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './BostonMap.css'
 import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
+import { DataContext } from '../context/DataContext';
 
 const containerStyle = {
     width: '100%',
@@ -28,12 +29,6 @@ const mapOptions = {
   ],
 };
 
-const markers = [
-  { lat: 42.3601, lng: -71.0589 },
-  { lat: 42.3331, lng: -71.0897 },
-  { lat: 42.3772, lng: -71.1167 },
-];
-
 const markerIcon = {
   path: 'M-8,0a8,8 0 1,0 16,0a8,8 0 1,0 -16,0',
   fillColor: '#FF0000',
@@ -43,6 +38,8 @@ const markerIcon = {
 };
 
 export default function InteractiveMap() {
+  const { markers } = useContext(DataContext)
+
   return (
     <div className = "Map">
         <LoadScript googleMapsApiKey = {process.env.REACT_APP_GOOGLE_MAPS_API_KEY} loadingElement={<div>Loading...</div>}>
