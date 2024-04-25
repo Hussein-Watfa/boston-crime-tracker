00f52b627202d36f1441d87e11b3cfef9f232b25
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-24 20:43:06 -0400

    formatting

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,7 +4,7 @@ import './TopBar.css'
 export default function TopBar() {
   return (
     <div className = "TopBar" >
-      <text>Boston Crime Tracker</text>
+      <text className = "text-TopBar">Boston Crime Tracker</text>
     </div>
   )
 }
