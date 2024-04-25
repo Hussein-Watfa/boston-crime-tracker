c7b5140bfc17ed7d968607491681cd624c7557bb
Hussein-Watfa <husseinwatfa101@gmail.com>
2024-04-24 21:49:32 -0400

    developed Results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './Results.css'
+import React, { useContext } from 'react';
+import './Results.css';
+import { DataContext } from '../context/DataContext';
 
 export default function Results() {
+  const { results } = useContext(DataContext);
+
   return (
-    <div className = "Results">
-      <text className = "text-Results">Top 5 Recent Cases</text>
+    <div className="Results">
+      <div className = "Results-container" >
+      <h2 className="text-Results">5 Most Recent Cases</h2>
+      <ul>
+        {results.map((result) => (
+          <li key={result.id}>{result.name}</li>
+        ))}
+      </ul>
+      </div>
     </div>
-  )
+  );
 }
